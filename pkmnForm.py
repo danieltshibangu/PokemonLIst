9827eb3ae7dade5de27d04aa7d1b5fce1eca865d
danieltshibangu <45724950+danieltshibangu@users.noreply.github.com>
2019-10-20 18:37:52 -0400

    Added the form page for pokemon.py
    This form page creates a function that allows access to fields within wtforms that can be used 
    in the website

---
+++ pkmnForm.py
@@ -0,0 +1,7 @@
+# import FlaskForm is just a wrapper that has useful methods for making secure form 
+from flask_wtf import FlaskForm
+# next we import fields from wtforms that allow data entry 
+# fields render and convert data 
+from wtforms import StringField, SubmitField, TextAreaField
+# import validators from wtf.validators for requiring data and email 
+from wtf.validators import 
