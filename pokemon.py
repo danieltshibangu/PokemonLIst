f1e1537e511477b5a3fda7f317b44bde19ed685d
Daniel Tshibangu <45724950+danieltshibangu@users.noreply.github.com>
2019-10-20 22:19:37 -0400

    Updates Pokemon.py
    The update adds pokemon to the list, handles input if entered pokemon has already been added to the list

--- pokemon.py
+++ pokemon.py
@@ -2,25 +2,28 @@ from flask import Flask, render_template, url_for, request
 # this uses the flask constructor to connect flask to the main output 
 app = Flask( __name__ )
 
-# make a list of popular cartoons 
-Pokemons =["Pikachu", "Charizard", "Squirtle", "Jigglypuff",
-			"Bulbasaur", "Gengar", "Charmander", "Mew", "Lugia", "Gyarados"]
+#configure a secret key for csrf protection
+# config loads the current configuration change to an object that 
+# affects all other extentions, files, etc 
+app.config[ "SECRET KEY"] = "WGNy;2An6=}_&Fod1Tq~D!4_lu3/7[c?H!xcJG[Lr77wQX66]4XJigy_;n6K"
+app.app_context() 
 
-# the route will connect to home at extension '/' or '/home'
-@app.route( '/' )
-# this definition contains the contents of the home page
-def home(): 
-	# this uses the html format from the html file as substitute
-	return render_template( 'practice_home.html', pokemon=Pokemons, length=len(Pokemons))
-'''
-@app.route( '/home', methods=[ 'POST', 'GET' ])
-def result():
-	if request.method == 'POST':
-		result = request.form
-		render_template( 'practice_home.html', result=result )
-'''
+# create a list that stores pokemon 
+pokemonList = [ "Pikachu", "Bulbasaur", "Squirtle", "Charmander" ]
 
-# call the main function under a conditional to avoid errors
-# run the program if the debugger comes back as true 
+# create a route for the site 
+app.route( 'pokeList', methods=[ 'POST', 'GET' ])
+def pokeList(): 
+	message = ""
+	if request.method = 'POST': 
+		pokemonName = request.form.get( "pokemon_Name" )
+		pokemonList.append( pokemonName )
+		message = "Pokemon added!"
+
+		if pokemonName in pokemonList:
+			message = "Pokemon already added!"
+
+	return render_template( "pkmnHTML.html", message=message )
+
 if __name__ == "__main__":
 	app.run( debug=True )
