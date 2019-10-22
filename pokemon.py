9cf409c3a70092ed5bf13fd84815f489c2783360
Daniel Tshibangu <45724950+danieltshibangu@users.noreply.github.com>
2019-10-21 23:13:01 -0400

    Updates Pokemon.py
    I'd been using an inefficient way of submitting input to a form
    
    In this doc, I've added ways to use the form in order to get data from the user in an efficient manner. 
    The names I get from of the user are stored in the form and passed to a variable called pokemonName 
    If the name entered is not in pokemonLIst List then it gets appended to it,  and a flash message is sent. 
    Otherwise, the name isn't appended to the list and it triggers a failure flash message. 
    
    The rendered page is then rendered with an accessible variable called form that represents the form of the 
    pkmnForm's classes and variables.

--- pokemon.py
+++ pokemon.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect, current_app, flash
+# get access to the form 
+from pkmnForm import pokemonSubmit
+
 # this uses the flask constructor to connect flask to the main output 
 app = Flask( __name__ )
 
@@ -8,22 +11,30 @@ app = Flask( __name__ )
 app.config[ "SECRET KEY"] = "WGNy;2An6=}_&Fod1Tq~D!4_lu3/7[c?H!xcJG[Lr77wQX66]4XJigy_;n6K"
 app.app_context() 
 
-# create a list that stores pokemon 
+# create a list that stores pokemon; includes sample pokemon already 
 pokemonList = [ "Pikachu", "Bulbasaur", "Squirtle", "Charmander" ]
 
 # create a route for the site 
-@app.route( 'pokeList', methods=[ 'POST', 'GET' ])
+@app.route( '/pokeList', methods=[ 'POST', 'GET' ])
 def pokeList(): 
-	message = ""
-	if request.method = 'POST': 
-		pokemonName = request.form.get( "pokemon_Name" )
-		pokemonList.append( pokemonName )
-		message = "Pokemon added!"
+	# create a form variable that initializes the class 
+	form = pokemonSubmit()
+	# this validate function returns True if form is submitted 
+	# using a POST request AND it is valid 
+	if form.validate_on_submit():
+		# from the form, the name variable inside the function, and data gets it 
+		pokemonName = form.name.data
 
-		if pokemonName in pokemonList:
-			message = "Pokemon already added!"
+		if pokemonName not in pokemonList: 
+			pokemonList.append( pokemonName )
+			flash( "Pokemon added!", 'success' )
+		else: 
+			flash( "Pokemon already added!", 'failure' ) 
+		
+		print( "\nData Recieved...Now redirecting.")
+		return redirect( request.url )
 
-	return render_template( "pkmnHTML.html", message=message )
+	return render_template( "pkmnHTML.html", form=form )
 
 if __name__ == "__main__":
 	app.run( debug=True )
